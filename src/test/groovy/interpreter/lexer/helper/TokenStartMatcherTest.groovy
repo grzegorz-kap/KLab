353e614a9f84e71c225d7f39df12f09a94f68d84
Grzegorz Kapica <grzk@outlook.com>
2015-09-02 18:33:57 +0200

    #6 Add TokenStartMatcherTest

--- src/test/groovy/interpreter/lexer/helper/TokenStartMatcherTest.groovy
+++ src/test/groovy/interpreter/lexer/helper/TokenStartMatcherTest.groovy
@@ -17,7 +17,7 @@ class TokenStartMatcherTest extends Specification {
             def result = tokenStartMatcher.isNumberStart();
         then:
             1 * tokenizerContext.charAt(0) >> '0'
-            result == true
+            result
     }
 
     def "Testing number which begins with dot"() {
@@ -27,14 +27,74 @@ class TokenStartMatcherTest extends Specification {
             1 * tokenizerContext.charAt(0) >> '.'
             1 * tokenizerContext.isCharAt(0, '.') >> true
             1 * tokenizerContext.charAt(1) >> '2'
-            result == true
+            result
 
         when:
             result = tokenStartMatcher.isNumberStart();
         then:
             1 * tokenizerContext.isCharAt(0, '.') >> true;
             1 * tokenizerContext.charAt(1) >> '.'
-            result == false;
+            !result
+    }
+
+    def "Testing is number start"() {
+        when:
+            def result = tokenStartMatcher.isWordStart()
+        then:
+            1 * tokenizerContext.charAt(0) >> 'f'
+            result
+
+        when:
+            result = tokenStartMatcher.isWordStart()
+        then:
+            1 * tokenizerContext.isCharAt(0, '_') >> true
+            result
+
+        when:
+            result = tokenStartMatcher.isWordStart()
+        then:
+            1 * tokenizerContext.isCharAt(0, '$') >> true
+            result
+
+        when:
+            result = tokenStartMatcher.isWordStart()
+        then:
+            1 * tokenizerContext.charAt(0) >> 3
+            !result
+    }
+
+    def "Testing isNewLineStart"() {
+        when:
+            def result = tokenStartMatcher.isNewLineStart()
+        then:
+            1 * tokenizerContext.isCharAt(0, '\n') >> true
+            result
+
+        when:
+            result = tokenStartMatcher.isNewLineStart()
+        then:
+            1 * tokenizerContext.isCharAt(0, '\n') >> false
+            !result
+    }
+
+    def "Testing isSpaceOrTabulatorStart"() {
+        when:
+            def result = tokenStartMatcher.isSpaceOrTabulatorStart()
+        then:
+            1 * tokenizerContext.isCharAt(0, ' ') >> true
+            result
+
+        when:
+            result = tokenStartMatcher.isSpaceOrTabulatorStart()
+        then:
+            1 * tokenizerContext.isCharAt(0, '\t') >> true
+            result
+
+        when:
+            result = tokenStartMatcher.isSpaceOrTabulatorStart()
+        then:
+            2 * tokenizerContext.isCharAt(0, _) >> false
+            !result
     }
 
 }
