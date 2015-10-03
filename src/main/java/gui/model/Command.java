11386e7084b035b3f3f05b3b9c2991d9bc5c8792
Grzegorz Kapica <grzk@outlook.com>
2015-10-03 13:02:19 +0200

    #67 Add command holder class

---
+++ src/main/java/gui/model/Command.java
@@ -0,0 +1,29 @@
+package gui.model;
+
+import java.time.LocalDateTime;
+
+public class Command {
+
+    private String content;
+    private LocalDateTime createdAt;
+
+    public Command() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
