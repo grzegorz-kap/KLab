5a1064bdfaebb6db5c8d5d55714f9ee3a60c8e8b
Grzegorz Kapica <grzk@outlook.com>
2015-10-03 20:39:06 +0200

    #75 Add sub matrix handler

---
+++ src/main/java/interpreter/core/arithmetic/matrix/ojalgo/OjalgoMatrixSubtractors.java
@@ -0,0 +1,31 @@
+package interpreter.core.arithmetic.matrix.ojalgo;
+
+import interpreter.commons.ObjectData;
+import interpreter.core.arithmetic.NumericObjectsSubtractor;
+import interpreter.math.matrix.ojalgo.OjalgoMatrix;
+import interpreter.parsing.model.NumericType;
+import org.ojalgo.matrix.store.MatrixStore;
+import org.springframework.stereotype.Component;
+
+@Component
+class OjalgoMatrixDoubleSubtractor extends AbstractOjalgoMatrixSubtractor<Double> {
+
+    @Override
+    public NumericType getSupportedType() {
+        return NumericType.MATRIX_DOUBLE;
+    }
+}
+
+abstract class AbstractOjalgoMatrixSubtractor<T extends Number>
+        extends AbstractOjalgoMatrixBinaryOperator<T> implements NumericObjectsSubtractor {
+
+    @Override
+    protected MatrixStore<T> operate(OjalgoMatrix<T> first, OjalgoMatrix<T> second) {
+        return first.getMatrixStore().subtract(second.getMatrixStore());
+    }
+
+    @Override
+    public ObjectData sub(ObjectData a, ObjectData b) {
+        return operate(a, b);
+    }
+}
