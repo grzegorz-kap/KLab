8aa000d72eb6b1e0f32d5bbe16347dd8b7de6bf0
Grzegorz Kapica <grzk@outlook.com>
2015-10-04 00:03:39 +0200

    #84 Refactor InterpreterService

---
+++ src/main/java/interpreter/core/Interpreter.java
@@ -0,0 +1,29 @@
+package interpreter.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Interpreter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Interpreter.class);
+
+    private InterpreterService interpreterService;
+
+    public void start(String input) {
+        LOGGER.info("\n{}", input);
+        interpreterService.resetCodeAndStack();
+        try {
+            interpreterService.startExecution(input);
+        } catch (RuntimeException ex) {
+            LOGGER.error("Execution failed", ex);
+        }
+    }
+
+    @Autowired
+    public void setInterpreterService(InterpreterService interpreterService) {
+        this.interpreterService = interpreterService;
+    }
+}
--- src/main/java/interpreter/core/InterpreterService.java
+++ src/main/java/interpreter/core/InterpreterService.java
@@ -1,43 +1,19 @@
 package interpreter.core;
 
-import interpreter.commons.utils.ExpressionPrinter;
-import interpreter.commons.utils.MacroInstructionPrinter;
-import interpreter.execution.service.ExecutionService;
 import interpreter.lexer.model.TokenList;
-import interpreter.lexer.service.Tokenizer;
 import interpreter.parsing.model.ParseToken;
 import interpreter.parsing.model.expression.Expression;
-import interpreter.parsing.service.Parser;
 import interpreter.translate.model.MacroInstruction;
-import interpreter.translate.service.InstructionTranslator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class InterpreterService {
+class InterpreterService extends AbstractInterpreterService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InterpreterService.class);
 
-    private ExecutionService executionService;
-    private Parser parser;
-    private Tokenizer tokenizer;
-    private InstructionTranslator instructionTranslator;
-    private ExpressionPrinter expressionPrinter;
-    private MacroInstructionPrinter macroInstructionPrinter;
-
-    public void start(String input) {
-        try {
-            startExecution(input);
-        } catch (RuntimeException ex) {
-            LOGGER.error("Execution failed", ex);
-        }
-    }
-
-    private void startExecution(String input) {
-        LOGGER.info("\n{}", input);
-        executionService.resetCodeAndStack();
+    public void startExecution(String input) {
         TokenList tokenList = tokenizer.readTokens(input);
         Expression<ParseToken> expression = parser.process(tokenList);
         LOGGER.info("\n{}", expressionPrinter.expressionToString(expression));
@@ -45,35 +21,5 @@ public class InterpreterService {
         LOGGER.info("\n{}", macroInstructionPrinter.print(macroInstruction));
         executionService.addInstructions(macroInstruction.getInstructions());
         executionService.start();
-    }
-
-    @Autowired
-    public void setExecutionService(ExecutionService executionService) {
-        this.executionService = executionService;
-    }
-
-    @Autowired
-    public void setTokenizer(Tokenizer tokenizer) {
-        this.tokenizer = tokenizer;
-    }
-
-    @Autowired
-    public void setParser(Parser parser) {
-        this.parser = parser;
-    }
-
-    @Autowired
-    public void setInstructionTranslator(InstructionTranslator instructionTranslator) {
-        this.instructionTranslator = instructionTranslator;
-    }
-
-    @Autowired
-    public void setExpressionPrinter(ExpressionPrinter expressionPrinter) {
-        this.expressionPrinter = expressionPrinter;
-    }
-
-    @Autowired
-    public void setMacroInstructionPrinter(MacroInstructionPrinter macroInstructionPrinter) {
-        this.macroInstructionPrinter = macroInstructionPrinter;
     }
 }
