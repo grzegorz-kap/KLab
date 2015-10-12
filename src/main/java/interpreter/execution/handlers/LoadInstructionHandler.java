c0b268e2fe8e5cadc75c6221f87152d3f9bb51f3
Grzegorz Kapica <grzk@outlook.com>
2015-10-12 16:05:49 +0200

    #106 Add undefined veriable exception

--- src/main/java/interpreter/execution/handlers/LoadInstructionHandler.java
+++ src/main/java/interpreter/execution/handlers/LoadInstructionHandler.java
@@ -1,6 +1,7 @@
 package interpreter.execution.handlers;
 
 import interpreter.commons.MemorySpace;
+import interpreter.execution.exception.UndefinedVariableException;
 import interpreter.execution.model.InstructionPointer;
 import interpreter.translate.model.instruction.InstructionCode;
 import interpreter.types.IdentifierObject;
@@ -10,18 +11,28 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class LoadInstructionHandler extends AbstractInstructionHandler {
 
+    public static final String VARIABLE_IS_NOT_DEFINED_MESSAGE = "Variable is not defined";
     private MemorySpace memorySpace;
 
     @Override
     public void handle(InstructionPointer instructionPointer) {
         IdentifierObject identifierObject = (IdentifierObject) instructionPointer.current().getObjectDate(0);
         ObjectData objectData = memorySpace.get(identifierObject.getAddress());
+        checkObjectData(objectData, identifierObject);
         executionContext.executionStackPush(objectData);
         instructionPointer.increment();
+    }
+
+    private void checkObjectData(ObjectData objectData, IdentifierObject identifierObject) {
+        if (Objects.isNull(objectData)) {
+            throw new UndefinedVariableException(VARIABLE_IS_NOT_DEFINED_MESSAGE, identifierObject);
+        }
     }
 
     @Override
--- src/main/java/interpreter/types/IdentifierObject.java
+++ src/main/java/interpreter/types/IdentifierObject.java
@@ -27,4 +27,8 @@ public class IdentifierObject implements ObjectData {
     public Integer getAddress() {
         return identifierToken.getAddress();
     }
+
+    public String getId() {
+        return identifierToken.getId();
+    }
 }
