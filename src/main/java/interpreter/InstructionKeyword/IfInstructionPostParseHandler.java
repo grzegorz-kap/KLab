f5a277e049867280dafc7f331f9f65d999d34e0f
Grzegorz Kapica <grzk@outlook.com>
2015-10-20 22:36:59 +0200

    #128 Add elseif support in translate layer

--- src/main/java/interpreter/InstructionKeyword/IfInstructionPostParseHandler.java
+++ src/main/java/interpreter/InstructionKeyword/IfInstructionPostParseHandler.java
@@ -30,7 +30,7 @@ public class IfInstructionPostParseHandler implements PostParseHandler {
 
 	@Override
 	public boolean canBeHandled(List<Expression<ParseToken>> expressions) {
-		return isIfStart(expressions) || isIfEnd(expressions) || isElse(expressions);
+		return isIfStart(expressions) || isIfEnd(expressions) || isElse(expressions) || isElseIf(expressions);
 	}
 
 	@Override
@@ -50,6 +50,9 @@ public class IfInstructionPostParseHandler implements PostParseHandler {
 		if (isElse(expressions)) {
 			return handleElse(expressions, instructionTranslator);
 		}
+		if(isElseIf(expressions)) {
+			return handleElseIf(expressions, instructionTranslator);
+		}
 		return new MacroInstruction();
 	}
 
@@ -57,24 +60,35 @@ public class IfInstructionPostParseHandler implements PostParseHandler {
 	public void setCode(Code code) {
 		this.code = code;
 	}
+	
+	private MacroInstruction handleElseIf(List<Expression<ParseToken>> expressions,
+			InstructionTranslator instructionTranslator) {
+		expressions.get(1).setProperty(Expression.PRINT_PROPERTY_KEY, false);
+		JumperInstruction jumperInstruction = createJumpOnFalse();
+		JumperInstruction jumpEndInstruction = createJmpInstruction();
+		setupOnFalseOrThrow(1);
+		ifInstructionContext.addEndIfJumper(jumpEndInstruction);
+		ifInstructionContext.setJumpOnFalse(jumperInstruction);
+		return new MacroInstruction()
+				.add(jumpEndInstruction)
+				.add(instructionTranslator.translate(expressions.get(1)))
+				.add(jumperInstruction);
+	}
 
 	private MacroInstruction handleIFStart(List<Expression<ParseToken>> expressions,
 			InstructionTranslator instructionTranslator) {
 		ifInstructionContext.addIf();
 		expressions.get(1).setProperty(Expression.PRINT_PROPERTY_KEY, false);
-		JumperInstruction jumperInstruction = new JumperInstruction();
-		jumperInstruction.setArgumentsNumber(0);
-		jumperInstruction.setInstructionCode(InstructionCode.JMPF);
+		JumperInstruction jumperInstruction = createJumpOnFalse();
 		ifInstructionContext.setJumpOnFalse(jumperInstruction);
 		return instructionTranslator.translate(expressions.get(1))
 				.add(jumperInstruction);
 	}
 
+
 	private MacroInstruction handleElse(List<Expression<ParseToken>> expressions,
 			InstructionTranslator instructionTranslator) {
-		JumperInstruction jumperInstruction = new JumperInstruction();
-		jumperInstruction.setArgumentsNumber(0);
-		jumperInstruction.setInstructionCode(InstructionCode.JMP);
+		JumperInstruction jumperInstruction = createJmpInstruction();
 		ifInstructionContext.addEndIfJumper(jumperInstruction);
 		setupOnFalseOrThrow(1);
 		expressions.get(0).setProperty(PRINT_PROPERTY_KEY, false);
@@ -89,6 +103,20 @@ public class IfInstructionPostParseHandler implements PostParseHandler {
 		expressions.get(0).setProperty(Expression.PRINT_PROPERTY_KEY, false);
 		ifInstructionContext.removeLastIf();
 		return new MacroInstruction();
+	}
+	
+	private JumperInstruction createJmpInstruction() {
+		JumperInstruction jumperInstruction = new JumperInstruction();
+		jumperInstruction.setArgumentsNumber(0);
+		jumperInstruction.setInstructionCode(InstructionCode.JMP);
+		return jumperInstruction;
+	}
+	
+	private JumperInstruction createJumpOnFalse() {
+		JumperInstruction jumperInstruction = new JumperInstruction();
+		jumperInstruction.setArgumentsNumber(0);
+		jumperInstruction.setInstructionCode(InstructionCode.JMPF);
+		return jumperInstruction;
 	}
 
 	private boolean isIfEnd(List<Expression<ParseToken>> expressions) {
@@ -106,7 +134,11 @@ public class IfInstructionPostParseHandler implements PostParseHandler {
 	private boolean isElse(List<Expression<ParseToken>> expressions) {
 		return expressions.size() == 1 && isParseClass(expressions, ParseClass.ELSE_KEYWORD);
 	}
-
+	
+	private boolean isElseIf(List<Expression<ParseToken>> expressions) {
+		return expressions.size() == 2 && isParseClass(expressions, ParseClass.ELSEIF_KEYWORD);
+	}
+	
 	private void setupJumpOnFalse() {
 		int addressToJump = code.size();
 		JumperInstruction jumpOnFalse = ifInstructionContext.getJumpOnFalse();
