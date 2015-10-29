b9c1b8939a4af8cd80966c97aea994595b22120e
Grzegorz Kapica <grzk@outlook.com>
2015-10-29 17:34:19 +0100

    #147 Add ans variable

--- src/main/java/interpreter/translate/handlers/OperatorTranslateHandler.java
+++ src/main/java/interpreter/translate/handlers/OperatorTranslateHandler.java
@@ -3,6 +3,7 @@ package interpreter.translate.handlers;
 import interpreter.parsing.model.ParseClass;
 import interpreter.parsing.model.ParseToken;
 import interpreter.parsing.model.expression.Expression;
+import interpreter.parsing.model.tokens.operators.OperatorCode;
 import interpreter.parsing.model.tokens.operators.OperatorToken;
 import interpreter.translate.factory.OperatorInstructionCodesFactory;
 import interpreter.translate.model.Instruction;
@@ -22,6 +23,9 @@ public class OperatorTranslateHandler extends AbstractTranslateHandler {
     @Override
     public void handle(Expression<ParseToken> expression) {
         OperatorToken operatorToken = (OperatorToken) expression.getValue();
+        if(OperatorCode.ASSIGN.equals(operatorToken.getOperatorCode())) {
+        	expression.setProperty(Expression.ANS_PROPERTY_KEY, false);
+        }
         InstructionCode instructionCode = operatorInstructionCodesFactory.get(operatorToken.getOperatorCode());
         Instruction instruction = new Instruction();
         instruction.setInstructionCode(instructionCode);
