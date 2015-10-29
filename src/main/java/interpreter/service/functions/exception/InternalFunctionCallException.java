95da5bd6d60cb0068d2f78677eed8f12352f7db3
Grzegorz Kapica <grzk@outlook.com>
2015-10-29 21:46:17 +0100

    Commit

--- src/main/java/interpreter/service/functions/exception/InternalFunctionCallException.java
+++ src/main/java/interpreter/service/functions/exception/InternalFunctionCallException.java
@@ -5,6 +5,7 @@ import interpreter.service.functions.InternalFunction;
 
 public class InternalFunctionCallException extends InterpreterException {
 	
+	private static final long serialVersionUID = 1L;
 	private InternalFunction internalFunction;
 	
 	public InternalFunctionCallException(InternalFunction internalFunction) {
@@ -14,6 +15,11 @@ public class InternalFunctionCallException extends InterpreterException {
 	public InternalFunctionCallException(InternalFunction internalFunction, Throwable cause) {
 		super(cause);
 		this.internalFunction = internalFunction;
+	}
+	
+	@Override
+	public String getMessage() {
+		return super.getMessage() + String.format(". Error in '%s' function call.", internalFunction.getName());
 	}
 
 }
