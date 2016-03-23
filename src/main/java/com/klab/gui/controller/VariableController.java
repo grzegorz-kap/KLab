c28276406bd5aa82999428af829938abd63a03a7
Grzegorz Kapica <grzk@outlook.com>
2016-03-23 23:30:54 +0100

    dynamic updates to variable list

--- src/main/java/com/klab/gui/controller/VariableController.java
+++ src/main/java/com/klab/gui/controller/VariableController.java
@@ -1,7 +1,9 @@
 package com.klab.gui.controller;
 
+import com.google.common.collect.Maps;
 import com.google.common.eventbus.Subscribe;
 import com.klab.interpreter.commons.memory.MemorySpace;
+import com.klab.interpreter.commons.memory.ObjectWrapper;
 import com.klab.interpreter.core.events.ExecutionCompletedEvent;
 import com.klab.interpreter.debug.BreakpointReachedEvent;
 import javafx.application.Platform;
@@ -17,47 +19,93 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 @Component
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class VariableController implements Initializable {
     private MemorySpace memorySpace;
+    private int currentScope = Integer.MIN_VALUE;
+    private Map<ObjectWrapper, Variable<TitledPane>> variablesMap = Maps.newHashMap();
 
     @FXML
     private VBox variablesBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 
     @Subscribe
     private void onExecutionComplateEvent(ExecutionCompletedEvent event) {
-        refreshVariables();
+        Platform.runLater(this::refreshVariables);
     }
 
     @Subscribe
     private void onBreakPointReachedEvent(BreakpointReachedEvent event) {
-        refreshVariables();
+        Platform.runLater(this::refreshVariables);
     }
 
     private void refreshVariables() {
-        Platform.runLater(() -> {
-            variablesBox.getChildren().clear();
-            memorySpace.listCurrentScopeVariables().forEach(variable -> {
-                Label text = new Label(variable.getData().toString());
-                text.setAlignment(Pos.TOP_LEFT);
+        boolean isNewScope = memorySpace.scopeId() != currentScope;
 
-                TitledPane titledPane = new TitledPane(variable.getData().getName(), text);
-                titledPane.setExpanded(false);
-                variablesBox.getChildren().add(titledPane);
-            });
-        });
+        if (isNewScope) {
+            variablesBox.getChildren().clear();
+            variablesMap.clear();
+        }
+
+
+        memorySpace.listCurrentScopeVariables()
+                .filter(this::isUpdated)
+                .peek(variable -> {
+                    Variable<TitledPane> var = variablesMap.remove(variable);
+                    if (var != null) {
+                        variablesBox.getChildren().remove(var.getNode());
+                    }
+                })
+                .map(variable -> {
+                    Label text = new Label(variable.getData().toString());
+                    text.setAlignment(Pos.TOP_LEFT);
+                    TitledPane titledPane = new TitledPane(variable.getData().getName(), text);
+                    titledPane.setExpanded(false);
+                    return new Variable<>(titledPane, variable);
+                })
+                .peek(var -> variablesBox.getChildren().add(var.getNode()))
+                .forEach(var -> variablesMap.put(var.getObjectWrapper(), var));
+        currentScope = memorySpace.scopeId();
+    }
+
+    private boolean isUpdated(ObjectWrapper w) {
+        Variable<TitledPane> variable = variablesMap.get(w);
+        return variable == null || variable.getVersion() != w.getVersion();
     }
 
     @Autowired
     public void setMemorySpace(MemorySpace memorySpace) {
         this.memorySpace = memorySpace;
+    }
+
+    private static class Variable<N> {
+        private N node;
+        private long version;
+        private ObjectWrapper objectWrapper;
+
+        Variable(N node, ObjectWrapper objectWrapper) {
+            this.node = node;
+            this.version = objectWrapper.getVersion();
+            this.objectWrapper = objectWrapper;
+        }
+
+        N getNode() {
+            return node;
+        }
+
+        ObjectWrapper getObjectWrapper() {
+            return objectWrapper;
+        }
+
+        long getVersion() {
+            return version;
+        }
     }
 }
