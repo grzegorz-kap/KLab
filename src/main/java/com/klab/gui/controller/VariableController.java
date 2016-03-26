c5f113fb55705d7c3d8a5ff04e81a24cc5d0e9a2
Grzegorz Kapica <grzk@outlook.com>
2016-03-26 19:02:42 +0100

    Display variables in TableView

--- src/main/java/com/klab/gui/controller/VariableController.java
+++ src/main/java/com/klab/gui/controller/VariableController.java
@@ -7,21 +7,32 @@ import com.klab.interpreter.commons.memory.MemorySpace;
 import com.klab.interpreter.commons.memory.ObjectWrapper;
 import com.klab.interpreter.core.events.ExecutionCompletedEvent;
 import com.klab.interpreter.debug.BreakpointReachedEvent;
+import com.klab.interpreter.types.Addressable;
+import com.klab.interpreter.types.Indexable;
+import com.klab.interpreter.types.ObjectData;
+import com.klab.interpreter.types.Sizeable;
+import com.klab.interpreter.types.matrix.Matrix;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 @Component
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
@@ -63,9 +74,31 @@ public class VariableController implements Initializable {
     }
 
     private Variable<TitledPane> createNew(ObjectWrapper variable) {
-        Label text = new Label(variable.getData().toString());
-        text.setAlignment(Pos.TOP_LEFT);
-        TitledPane titledPane = new TitledPane(variable.getData().getName(), text);
+        TableView<VariableRow> tableView = new TableView<>();
+
+        long rows = ((Sizeable) variable.getData()).getRows();
+        long columns = ((Sizeable) variable.getData()).getColumns();
+
+        LongStream.range(0L, rows)
+                .mapToObj(m -> new VariableRow(m, variable.getData()))
+                .forEach(row -> tableView.getItems().add(row));
+
+        LongStream.range(0L, columns).forEach(n -> {
+            TableColumn<VariableRow, String> column = new TableColumn<>();
+            column.setText(String.valueOf(n+1));
+            column.setCellValueFactory(param -> {
+                ObjectData objectData = param.getValue().getObjectData();
+                if(objectData instanceof Matrix) {
+                    Matrix matrix = (Matrix) objectData;
+                    return new SimpleStringProperty(matrix.get(param.getValue().getRowNumber(), n).toString());
+                } else {
+                    return new SimpleStringProperty(objectData.toString());
+                }
+            });
+            tableView.getColumns().add(column);
+        });
+
+        TitledPane titledPane = new TitledPane(variable.getData().getName(), tableView);
         titledPane.setExpanded(false);
         return new Variable<>(titledPane, variable);
     }
@@ -77,13 +110,31 @@ public class VariableController implements Initializable {
         }
     }
 
-    private boolean isUpdated(ObjectWrapper w) {
-        Variable<TitledPane> variable = variablesMap.get(w);
-        return variable == null || variable.getVersion() != w.getVersion();
+    private boolean isUpdated(ObjectWrapper oldOne) {
+        Variable<TitledPane> newOne = variablesMap.get(oldOne);
+        return newOne == null || newOne.getVersion() != oldOne.getVersion();
     }
 
     @Autowired
     public void setMemorySpace(MemorySpace memorySpace) {
         this.memorySpace = memorySpace;
+    }
+
+    private static class VariableRow {
+        private long rowNumber;
+        private ObjectData objectData;
+
+        public VariableRow(long rowNumber, ObjectData objectData) {
+            this.rowNumber = rowNumber;
+            this.objectData = objectData;
+        }
+
+        public long getRowNumber() {
+            return rowNumber;
+        }
+
+        public ObjectData getObjectData() {
+            return objectData;
+        }
     }
 }
--- src/main/java/com/klab/interpreter/types/scalar/Scalar.java
+++ src/main/java/com/klab/interpreter/types/scalar/Scalar.java
@@ -2,7 +2,13 @@ package com.klab.interpreter.types.scalar;
 
 import com.klab.interpreter.types.*;
 
-public interface Scalar<T extends Number> extends Sizeable, NumericObject, Negable<Scalar<T>>, Editable<T>, EditSupportable<T> {
+public interface Scalar<T extends Number> extends
+        Sizeable,
+        NumericObject,
+        Negable<Scalar<T>>,
+        Editable<T>,
+        EditSupportable<T>
+{
     T getValue();
 
     int getIntOrThrow();
