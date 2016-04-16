49eb705b0c75170e711fd85e950f0c6b85cee2c4
Grzegorz Kapica <grzk@outlook.com>
2016-04-16 13:39:38 +0200

    Remove profiling mode switch event

--- src/main/java/com/klab/interpreter/core/InterpreterService.java
+++ src/main/java/com/klab/interpreter/core/InterpreterService.java
@@ -1,7 +1,5 @@
 package com.klab.interpreter.core;
 
-import com.google.common.eventbus.Subscribe;
-import com.klab.gui.events.ProfilingModeSwitchEvent;
 import com.klab.interpreter.core.code.CodeGenerator;
 import com.klab.interpreter.execution.service.ExecutionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,15 +21,6 @@ class InterpreterService {
     private void execute() {
         if (codeGenerator.executionCanStart()) {
             executionService.start();
-        }
-    }
-
-    @Subscribe
-    private void onProfilingModeSwitch(ProfilingModeSwitchEvent event) {
-        if (event.getData()) {
-            executionService.enableProfiling();
-        } else {
-            executionService.disableProfiling();
         }
     }
 
