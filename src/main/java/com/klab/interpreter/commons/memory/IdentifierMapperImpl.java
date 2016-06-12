ea0d9ce767c609fe1acff65a46cb9ea51d507adb
Grzegorz Kapica <grzk@outlook.com>
2016-06-12 15:07:40 +0200

    Code polish

--- src/main/java/com/klab/interpreter/commons/memory/IdentifierMapperImpl.java
+++ src/main/java/com/klab/interpreter/commons/memory/IdentifierMapperImpl.java
@@ -3,16 +3,18 @@ package com.klab.interpreter.commons.memory;
 import com.google.common.collect.Maps;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
 
 @Service
 public class IdentifierMapperImpl implements IdentifierMapper {
     private Integer mainIdentifierFreeAddress = 0;
     private Integer internalFunctionFreeAddress = 0;
     private Integer externalFunctionFreeAddress = 0;
-    private Map<String, Integer> mainIdentifierAddressMap = new HashMap<>();
-    private Map<String, Integer> internalFunctionAddressMap = new HashMap<>();
-    private Map<String, Integer> externalFunctionAddressMap = new HashMap<>();
+    private Map<String, Integer> mainIdentifierAddressMap = Maps.newHashMap();
+    private Map<String, Integer> internalFunctionAddressMap = Maps.newHashMap();
+    private Map<String, Integer> externalFunctionAddressMap = Maps.newHashMap();
 
     private Deque<Map<String, Integer>> scopes = new ArrayDeque<>();
     private Deque<Integer> scopesAddress = new ArrayDeque<>();
@@ -37,50 +39,27 @@ public class IdentifierMapperImpl implements IdentifierMapper {
     }
 
     @Override
+    public Integer registerExternalFunction(String id) {
+        return externalFunctionAddressMap.computeIfAbsent(id, name -> externalFunctionFreeAddress++);
+    }
+
+    @Override
+    public Integer registerMainIdentifier(String id) {
+        return mainIdentifierAddressMap.computeIfAbsent(id, name -> mainIdentifierFreeAddress++);
+    }
+
+    @Override
+    public Integer registerInternalFunction(String id) {
+        return internalFunctionAddressMap.computeIfAbsent(id, name -> internalFunctionFreeAddress++);
+    }
+
+    @Override
     public Integer getMainAddress(String id) {
         return mainIdentifierAddressMap.get(id);
     }
 
     @Override
-    public Integer registerExternalFunction(String id) {
-        Integer address = externalFunctionAddressMap.get(id);
-        if (address == null) {
-            address = externalFunctionFreeAddress++;
-            externalFunctionAddressMap.put(id, address);
-        }
-        return address;
-    }
-
-    @Override
     public Integer getExternalAddress(String id) {
         return externalFunctionAddressMap.get(id);
-    }
-
-    @Override
-    public Integer registerMainIdentifier(String id) {
-        Integer address = mainIdentifierAddressMap.get(id);
-        if (Objects.isNull(address)) {
-            address = getNextFreeMainAddressAndIncrement();
-            mainIdentifierAddressMap.put(id, address);
-        }
-        return address;
-    }
-
-    @Override
-    public Integer registerInternalFunction(String id) {
-        Integer address = internalFunctionAddressMap.get(id);
-        if (Objects.isNull(address)) {
-            address = getNextFreeInternalFunctionFreeAddressAndIncrement();
-            internalFunctionAddressMap.put(id, address);
-        }
-        return address;
-    }
-
-    private Integer getNextFreeMainAddressAndIncrement() {
-        return mainIdentifierFreeAddress++;
-    }
-
-    private Integer getNextFreeInternalFunctionFreeAddressAndIncrement() {
-        return internalFunctionFreeAddress++;
     }
 }
