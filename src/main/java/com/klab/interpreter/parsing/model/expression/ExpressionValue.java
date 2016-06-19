a2bd02543943467c80f273350274e48a7fedfed5
Grzegorz Kapica <grzk@outlook.com>
2016-06-19 18:53:15 +0200

    Refactor expression classes

--- src/main/java/com/klab/interpreter/parsing/model/expression/ExpressionValue.java
+++ src/main/java/com/klab/interpreter/parsing/model/expression/ExpressionValue.java
@@ -1,12 +1,25 @@
 package com.klab.interpreter.parsing.model.expression;
 
+import java.util.Collection;
+import java.util.List;
+
 public class ExpressionValue<T> extends AbstractExpression<T> implements Expression<T> {
     public ExpressionValue(T value) {
-        this.value = value;
+        setValue(value);
     }
 
     @Override
     public int getChildrenCount() {
         return 0;
+    }
+
+    @Override
+    public void addChildren(Collection<? extends Expression<T>> expressions) {
+        throw new UnsupportedOperationException("addChildren");
+    }
+
+    @Override
+    public List<Expression<T>> getChildren() {
+        throw new UnsupportedOperationException("addChildren");
     }
 }
